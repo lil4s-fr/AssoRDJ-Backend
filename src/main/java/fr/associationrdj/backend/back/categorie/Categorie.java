7c5918cb60ca500b8338c732a1e6f643a1ab0a05
Dorian_TERNISIEN <dternisien59@gmail.com>
2023-05-30 16:05:54 +0200

    Creation du modele du repository et du service de categorie

---
+++ src/main/java/fr/associationrdj/backend/back/categorie/Categorie.java
@@ -0,0 +1,20 @@
+package fr.associationrdj.backend.back.categorie;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Categorie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private Reservation reservation;
+    private String nom;
+    private String description;
+}
