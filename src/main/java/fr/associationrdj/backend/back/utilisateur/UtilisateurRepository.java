ed9f3f8dc04c588a3c424b1cbabb14803f5a4bc4
antoineDIas <antoine.dias.dunkerque@gmail.com>
2023-05-30 15:16:53 +0200

    Modification package utilisateur

--- src/main/java/fr/associationrdj/backend/back/utilisateur/UtilisateurRepository.java
+++ src/main/java/fr/associationrdj/backend/back/utilisateur/UtilisateurRepository.java
@@ -1,2 +1,10 @@
-package fr.associationrdj.backend.back.utilisateur;public interface UtilisateurRepository {
+package fr.associationrdj.backend.back.utilisateur;
+
+import jdk.jshell.execution.Util;
+import org.hibernate.sql.ast.tree.expression.JdbcParameter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
 }
