7563af6ba174b4f54ad7737ab5c7fd62f52dadd4
Dorian_TERNISIEN <dternisien59@gmail.com>
2023-05-30 13:43:04 +0200

    Creation de ArticleRepository

---
+++ src/main/java/fr/associationrdj/backend/back/article/ArticleRepository.java
@@ -0,0 +1,8 @@
+package fr.associationrdj.backend.back.article;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
