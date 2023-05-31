704511181678d8efce719707b539a399651ff66d
antoineDIas <antoine.dias.dunkerque@gmail.com>
2023-05-31 09:30:21 +0200

    Creation du package commentaire

--- src/main/java/fr/associationrdj/backend/back/commentaire/Commentaire.java
+++ src/main/java/fr/associationrdj/backend/back/commentaire/Commentaire.java
@@ -1,5 +1,7 @@
 package fr.associationrdj.backend.back.commentaire;
 
+import fr.associationrdj.backend.back.article.Article;
+import fr.associationrdj.backend.back.utilisateur.Utilisateur;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,7 +15,10 @@ public class Commentaire {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+    private Utilisateur utilisateur;
+    private Article article;
+    private String commentaire;
+    private String like_dislike;
 
 
 }
---
+++ src/main/java/fr/associationrdj/backend/back/commentaire/CommentaireRepository.java
@@ -0,0 +1,8 @@
+package fr.associationrdj.backend.back.commentaire;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentaireRepository extends JpaRepository<Commentaire, Long> {
+}
