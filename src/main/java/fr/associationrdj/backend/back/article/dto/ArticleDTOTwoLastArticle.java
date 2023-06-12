098a0acbdd8712e401215078df6b5b70d6776d7f
Dorian_TERNISIEN <dternisien59@gmail.com>
2023-06-12 11:45:14 +0200

    ajout d'une methode pour récupérer les 2 derniers articles

--- src/main/java/fr/associationrdj/backend/back/article/dto/ArticleDTOTwoLastArticle.java
+++ src/main/java/fr/associationrdj/backend/back/article/dto/ArticleDTOTwoLastArticle.java
@@ -3,14 +3,12 @@ package fr.associationrdj.backend.back.article.dto;
 import fr.associationrdj.backend.back.categorie.Categorie;
 import fr.associationrdj.backend.back.utilisateur.Utilisateur;
 import lombok.Data;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Data
-public class ArticleDTOThreeLastArticle {
+public class ArticleDTOTwoLastArticle {
     private Long id;
     private String titre;
     private List<Categorie> categories;
